88e09c7e88424571bd4ec710e977e3b37dbbae36
madde <madde@02f6a92ab9d0>
2019-06-13 19:27:00 +0000

    Using coraData instead of bookkeeper data

--- src/main/java/module-info.java
+++ src/main/java/module-info.java
@@ -1,5 +1,6 @@
 module se.uu.ub.cora.gatekeeper {
 	requires transitive se.uu.ub.cora.bookkeeper;
+	requires transitive se.uu.ub.cora.data;
 
 	exports se.uu.ub.cora.gatekeeper.user;
 }
--- src/main/java/se/uu/ub/cora/gatekeeper/user/UserStorage.java
+++ src/main/java/se/uu/ub/cora/gatekeeper/user/UserStorage.java
@@ -1,5 +1,5 @@
 /*
- * Copyright 2017, 2018 Uppsala University Library
+ * Copyright 2017, 2018, 2019 Uppsala University Library
  *
  * This file is part of Cora.
  *
@@ -19,7 +19,7 @@
 
 package se.uu.ub.cora.gatekeeper.user;
 
-import se.uu.ub.cora.bookkeeper.data.DataGroup;
+import se.uu.ub.cora.data.DataGroup;
 
 public interface UserStorage {
 
